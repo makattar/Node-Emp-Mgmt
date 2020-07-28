4b5b3a8bf5d9b405e0f02f44d1c3577b24d8cc1a
makattar <66944688+makattar@users.noreply.github.com>
2020-07-28 15:35:01 +0530

    Directly edited sequalize connection

--- config/database.js
+++ config/database.js
@@ -21,14 +21,8 @@ require('dotenv').config();
 
 //Testing ORM
 
-module.exports = new Sequelize(process.env.DB_URL,
+module.exports = new Sequelize(process.env.DATABASE_URL,
 {
   dialect: 'postgres',
-  protocol: 'postgres',
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false // <<<<<<< YOU NEED THIS
-    }
-  }
+  protocol: 'postgres'
 });
